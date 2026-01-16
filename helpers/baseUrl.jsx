6fc23c938277d81b9adefd5bc3ec5c5c0c27538c
SUPERIORII <adukuly461@gmail.com>
2026-01-16 12:28:23 +0000

    changes

--- helpers/baseUrl.jsx
+++ helpers/baseUrl.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const baseUrl = axios.create({
-  baseURL: "" || process.env.NEXT_PUBLIC_BASE_URL, //same ORIGIN,
+  baseURL: "", //same ORIGIN,
   withCredentials: true,
 });
 
